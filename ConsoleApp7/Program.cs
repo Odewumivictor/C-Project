6f6181d621474f9622d6bfec4a4d6fd04abf1911
odewumivictor <vickidubi1@gmail.com>
2022-08-31 16:07:26 +0100

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            int[] apcfans = { 23, 17, 21, 31, 27, 18 };
+
+            int total = 0;
+            foreach (var youthAge in apcfans)
+            {
+                total = total + youthAge;
+               
+            }
+            Console.WriteLine($"the sum of apsfans is {total}");
+            int sum = total;
+            Console.ReadLine();
+        }
+       
+    }
+}
